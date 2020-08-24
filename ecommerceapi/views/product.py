d265b43b3197b217aa5bca3e080542ead591abe6
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-08-24 16:01:45 -0500

    wip

--- ecommerceapi/views/product.py
+++ ecommerceapi/views/product.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from ecommerceapi.models import Product
+from ecommerceapi.models import Product, ProductType
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for products
@@ -25,6 +25,26 @@ class Products(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
+    
+    def create(self, request):
+        newsell = Product()
+        product_type = ProductType.objects.get(pk=request.data["producttype_id"])
+
+        newsell.product_type = product_type
+        newsell.title = request.data["title"]
+        newsell.customer_id = request.data["customer_id"]
+        newsell.price = request.data["price"]
+        newsell.description = request.data["description"]
+        newsell.quantity = request.data["quantity"]
+        newsell.location = request.data["location"]
+        newsell.image_path = request.data["image_path"]
+        newsell.created_at = request.data["created_at"]
+        newsell.save()
+
+        serializer = ProductSerializer(newsell, context={'request': request})
+
+        return Response(serializer.data)
+        
 
     def list(self, request):
         products = Product.objects.all()
